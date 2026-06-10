a58796d5d28d40e4e533d99521b3f5782260e3ca
Leonardo Chávez <n00449821pn.pe>
2026-06-10 09:16:05 -0500

    Creacion de variables de Usuario

--- Clases/Usuario.cs
+++ Clases/Usuario.cs
@@ -2,5 +2,13 @@
 {
     public class Usuario
     {
+        public string Nombre;
+        public string NombreUsuario;
+        public string Contrasena;
+        public override string ToString()
+        {
+            return NombreUsuario;
+        }
     }
+
 }
