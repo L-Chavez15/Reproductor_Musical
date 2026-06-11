ff2b3406e9eb0a0fb1cd71373069ac9b1b42cf68
Leonardo Chávez <n00449821pn.pe>
2026-06-10 22:42:24 -0500

    creacion de logica de usuarios

--- Clases/Arbol.cs
+++ Clases/Arbol.cs
@@ -9,9 +9,56 @@ namespace Clases
 {
     public class Arbol
     {
-        
-        
-        
+        public Nodo raizPrincipal2 = null;
+
+        public void InsertarUsuario(ref Nodo raiz, Usuario u)
+        {
+            if (raiz == null)
+            {
+                Nodo nuevo = new Nodo();
+                nuevo.datoUsuario = u;
+                raiz = nuevo;
+            }
+            else
+            {
+                if (string.Compare(u.NombreUsuario, raiz.datoUsuario.NombreUsuario) < 0)
+                {
+                    InsertarUsuario(ref raiz.izquierda, u);
+                }
+                else if (string.Compare(u.NombreUsuario, raiz.datoUsuario.NombreUsuario) > 0)
+                {
+                    InsertarUsuario(ref raiz.derecha, u);
+                }
+                else
+                {
+                    MessageBox.Show("Usuario ya registrado");
+                }
+            }
+        }
+        public Usuario BuscarUsuario(Nodo raiz, string nombreUsuario)
+        {
+            if (raiz == null)
+            {
+                return null;
+            }
+            else
+            {
+                if (string.Compare(nombreUsuario, raiz.datoUsuario.NombreUsuario) < 0)
+                {
+                    return BuscarUsuario(raiz.izquierda, nombreUsuario);
+                }
+                else if (string.Compare(nombreUsuario, raiz.datoUsuario.NombreUsuario) > 0)
+                {
+                    return BuscarUsuario(raiz.derecha, nombreUsuario);
+                }
+                else
+                {
+                    return raiz.datoUsuario;
+                }
+            }
+        }
+
+
         /*public void Eliminar(ref Nodo raiz, string nombre)
         {
             if (raiz == null)
